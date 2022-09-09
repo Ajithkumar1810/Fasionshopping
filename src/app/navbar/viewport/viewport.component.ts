e30f176a04f9d7011a8505e7d35bfca66b9fc642
Ajithkumar1810 <jaganajith18@gmail.com>
2022-09-09 15:22:47 +0530

    5 commit

--- src/app/navbar/viewport/viewport.component.ts
+++ src/app/navbar/viewport/viewport.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewport.component.css']
 })
 export class ViewportComponent implements OnInit {
- public menuoption=false;
+ public menuoption=!false;
   constructor() { }
 
   ngOnInit(): void {
